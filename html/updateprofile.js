b3c452f59bf584ac590c286b7d07e0fe9b77f9ae
Kade Dancy <kadedancy@gmail.com>
2024-02-08 15:19:12 -0500

    Updated code

--- html/updateprofile.js
+++ html/updateprofile.js
@@ -1,41 +1,28 @@
 "use strict";
 
-//just send data to server in background.
-//we don't care about the server's reply
-fetch("/profile/", {method: "POST", body: "..."} );
-
-//send data to server and wait for server to get it
-fetch("/profile/", {method: "POST", body: "..."} ).then( () => {
-    console.log("Got it");
-});
-
-//send data to server and wait for server to get it
-//and print server's response
-fetch("/profile/", {method: "POST", body: "..."} ).then( (resp) => {
-    resp.text().then( (txt) => {
-        console.log("Server said:",txt);
-    });
-});
-
-function submit(){
+function submit() {
     let fname = document.getElementById("fname").value;
     let lname = document.getElementById("lname").value;
     let dob = document.getElementById("birthdate").value;
-    let J = {
-        firstName: fname,
-        lastName: lname,
-        birthDate: dob
-    };
-    fetch( "/updateprofile",
-        {   method: "POST",
-            body: JSON.stringify(J)
-        }
-    ).then( (resp) => {
-        //can also use text(), blob(), or arrayBuffer()
-        resp.json().then( (J) => {
-            console.log("Server said:",J);
-        });
-    }).catch( (err) => {
-        console.log("Uh oh",err);
+    let profilePicInput = document.getElementById("picture");
+
+    let formData = new FormData();
+    formData.append("firstName", fname);
+    formData.append("lastName", lname);
+    formData.append("birthDate", dob);
+    formData.append("picture", profilePicInput.files[0]); // Assuming only one file is selected
+
+    fetch("/updateprofile", {
+        method: "POST",
+        body: formData,
     })
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        console.log("Server said:", data);
+    })
+    .catch((err) => {
+        console.log("Uh oh", err);
+    });
 }
--- src/updateprofile.py
+++ src/updateprofile.py
@@ -1,13 +1,29 @@
 import tornado.web
 import json
+import os
+import uuid 
 
 class Handler(tornado.web.RequestHandler):
-    def post(self):
-        J=json.loads(self.request.body)
-        firstName = J["firstName"]
-        lastName = J["lastName"]
-        dob = J["birthDate"]
-        print("WE GOT:",firstName,lastName,dob)
+    async def post(self):
+        try:
+            J = json.loads(self.request.body)
+            firstName = J["firstName"]
+            lastName = J["lastName"]
+            dob = J["birthDate"]
+            picture = J["picture"]
 
-        resp={"ok": True}
-        self.write( json.dumps(resp) )
+            picture_file = self.request.files.get("picture", [])
+            if picture_file:
+                uploaded_file = picture_file[0]
+                picture_content = uploaded_file["body"]
+                
+                picture_filename = str(uuid.uuid4()) + os.path.splitext(uploaded_file["filename"])[1]
+
+                save_path = os.path.join("../html", picture_filename)
+                with open(save_path, "wb") as f:
+                    f.write(picture_content)
+
+            print("WE GOT:", firstName, lastName, dob)
+            resp = {"ok": True}
+
+        self.write(json.dumps(resp))
